0aeab345e5b26069c50bc8785a25f90573ea3252
BarbaraDodsDev <barbaradods120@gmail.com>
2025-04-28 22:58:36 -0300

    Opção de saida para o Menu
    Foi adicionada uma opção para que o usuário retorne ao menu quando nenhuma das opções anteriores o atender

--- Cartorio.cpp
+++ Cartorio.cpp
@@ -127,8 +127,8 @@ int main()
 		printf("Escolha a opção desejada do menu\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); 
-		printf("Opção: ");//fim do menu
+		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do Sistema\n");
 				
 		scanf("%d", &opcao); //armazenando a escolha do usuário
 		
@@ -148,6 +148,11 @@ int main()
 			deletar();
 			break;
 		
+			case 4:
+			printf("Obrigada por utilizar o sistema!\n");
+			return 0;
+			break;
+			
 			default:
 			printf("Essa opção não está disponivel!\n");
 			system("pause");
